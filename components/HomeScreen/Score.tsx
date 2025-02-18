9f9a37a1bc30a4b20ba86d85273cc0b07786935e
Azeem Idrisi <azeemidrisi@protonmail.com>
2025-02-18 18:32:33 +0530

    Image animation complete

--- components/HomeScreen/Score.tsx
+++ components/HomeScreen/Score.tsx
@@ -1,5 +1,11 @@
-import { View, Text, ImageBackground } from "react-native";
-import React, { useContext } from "react";
+import {
+  View,
+  Text,
+  ImageBackground,
+  Animated,
+  Dimensions,
+} from "react-native";
+import React, { useContext, useEffect, useRef } from "react";
 import ProgressBar from "./ProgressBar";
 import { UserContext } from "../../store/user-context";
 
@@ -30,14 +36,32 @@ const Score = () => {
     bmi,
     healthData.sleep
   );
+
+  const slideAnim = useRef(new Animated.Value(-100)).current; // Start off-screen to the left
+  const screenWidth = Dimensions.get("window").width;
+
+  useEffect(() => {
+    const startAnimation = () => {
+      slideAnim.setValue(-300);
+
+      Animated.timing(slideAnim, {
+        toValue: screenWidth,
+        duration: 3000,
+        useNativeDriver: true,
+      }).start(() => startAnimation());
+    };
+
+    startAnimation();
+  }, []);
+
   return (
     <View className="p-5">
-      <ImageBackground
-        imageStyle={{ paddingBottom: 45, paddingTop: 20 }}
-        resizeMode="contain"
-        source={require("../../assets/translucent_logo.png")}
-        className="gap-y-12"
-      >
+      <View className="gap-y-12 relative">
+        <Animated.Image
+          className="size-64 absolute z-10 inset-0 top-5"
+          source={require("../../assets/translucent_logo.png")}
+          style={{ transform: [{ translateX: slideAnim }] }}
+        />
         <Text className="text-white/60 text-lg">Health Score</Text>
         <View className="gap-y-3">
           <Text className="text-white text-5xl font-bold">
@@ -48,7 +72,7 @@ const Score = () => {
           </Text>
         </View>
         <ProgressBar progressVal={Number.isNaN(score) ? 0 : score} />
-      </ImageBackground>
+      </View>
     </View>
   );
 };
